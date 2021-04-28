2c2ad8a648dd3c808af0d3d83f2ce7152fb1d98b
MathieuSoysal <Mathieu.soysal@outlook.fr>
2021-04-28 17:21:11 +0200

    🔨 update Connection.ts

--- src/ts/main/Connection.ts
+++ src/ts/main/Connection.ts
@@ -1,6 +1,8 @@
+import { graph, GraphCustom } from './graph-gestionnaire/GraphCustom';
+import { CustomWarn, SetProperties } from './InterfaceAndMisc';
 var webSocket: WebSocket;
 
-enum SageCommand {
+export enum SageCommand {
     propertiesRequest = "Properties",
     strongOrientationRequest = "strongOrientation",
     randomOrientationRequest = "randomOrientation",
@@ -10,7 +12,7 @@ enum SageCommand {
     closeConnection = "closeConnection"
 }
 
-function InitWebSocketConnection() {
+export function InitWebSocketConnection() {
     // Connect to Web Socket
     webSocket = new WebSocket("ws://localhost:9001/");
     // Set event handlers.
@@ -19,32 +21,38 @@ function InitWebSocketConnection() {
     };
 
     webSocket.onmessage = function (message) {
-        TreatResponse(StringToObject(message.data));
+        TreatResponse(eval(message.data));
     };
 
     webSocket.onclose = function () { };
 
-    webSocket.onerror = function (error) {
+    webSocket.onerror = function () {
         CustomWarn("Fail to connect with SageMath");
     };
 }
 
-function TreatResponse(response) {
+function TreatResponse(response: { request: SageCommand; result: string | any[]; }) {
     switch (response.request) {
         case SageCommand.propertiesRequest:
-            SetProperties(response.result[0], response.result[1], response.result[2], response.result[3], response.result[4]);
+            SetProperties(
+                response.result[0],
+                response.result[1],
+                response.result[2],
+                response.result[3],
+                response.result[4]
+            );
             break;
         case SageCommand.vertexColoringRequest:
-            SetNodesColoration(response.result);
+            graph.SetNodesColoration(response.result as any[]);
             break;
         case SageCommand.edgeColoringRequest:
-            SetLinksColoration(response.result)
+            graph.SetLinksColoration(response.result)
             break;
         case SageCommand.strongOrientationRequest:
-            InitNewGraph(StringToObject(response.result));
+            GraphCustom.setNewGraph(eval(response.result as string));
             break;
         case SageCommand.randomOrientationRequest:
-            InitNewGraph(StringToObject(response.result));
+            GraphCustom.setNewGraph(eval(response.result as string));
             break;
         case SageCommand.convertGraph:
             CustomWarn("Graph : " + response.result + " open in new Window");
@@ -58,35 +66,18 @@ function TreatResponse(response) {
     }
 }
 
-function RequestVertexColoring() {
-    SubmitMessage(SageCommand.vertexColoringRequest);
+
+
+export function SubmitMessage(parameter: SageCommand, message = "") {
+    graph.parameter = parameter;
+    graph.message = message;
+    webSocket.send(graph.toJSON());
 }
 
-function RequestEdgeColoring() {
-    SubmitMessage(SageCommand.edgeColoringRequest);
-}
-
-function RequestStrongOrientation() {
-    SubmitMessage(SageCommand.strongOrientationRequest);
-}
-
-function RequestRandomOrientation() {
-    SubmitMessage(SageCommand.randomOrientationRequest);
-}
-
-function RequestConvertGraph() {
-    SubmitMessage(SageCommand.convertGraph);
-}
-
-
-function SubmitMessage(parameter: SageCommand, message = "") {
-    graphJSON.parameter = parameter;
-    graphJSON.message = message;
-    var prettyJSON = PrettifyJSON();
-    webSocket.send(prettyJSON);
-}
-
-function onCloseClick() {
+export function onCloseClick() {
     webSocket.close();
 }
 
+export function UpdateGraphProperties(message = "") {
+    SubmitMessage(SageCommand.propertiesRequest, message);
+}
